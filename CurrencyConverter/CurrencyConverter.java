4731ca319b6ca179582df07a87a0e5ccb28cd053
Lika <shelley.bass.li@gmail.com>
2022-05-20 14:15:53 +0300

    Currency Converter step 2 completed
    Завершение второй части программы по переводу валюты с датской кроны на рубли. Добавлена проверка склонений.

--- CurrencyConverter/CurrencyConverter.java
+++ CurrencyConverter/CurrencyConverter.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
     public static void main(String[] args) {
-        final double ROUBLE_PER_DKRONE = 13.82; // курс покупки датской кроны
+        final double ROUBLE_PER_DKRONER = 13.82; // курс покупки датской кроны
         int dkk; // сумма денег в датских кронах
         double rub; // сумма денег в российских рублях
 
@@ -19,9 +19,29 @@ public class CurrencyConverter {
         dkk = into.nextInt();
 
         // Вычисляем сумму в рублях
-        rub = ROUBLE_PER_DKRONE * dkk;
+        rub = ROUBLE_PER_DKRONER * dkk;
 
-        // Отображаем результат:
-        System.out.println("При переводе на рубли вы получите сумму: " + rub);
+        // Проверяем склонение и отображаем результат
+        checkDeclination(dkk);
+        System.out.println(" при переводе на рубли будет равна " + rub + ".");
+    }
+
+    /*
+        Проект. Часть 2.
+        Добавление в проект структуры выбора для принятия решений
+        об окончаниях входной валюты в зависимости от её значений.
+     */
+    public static void checkDeclination(int kroner) {
+        int digitOnes = kroner % 10; // цифра разряда единиц
+        int digitTens = kroner % 100 / 10; // цифра разряда десятков
+
+        if (digitOnes == 1 && digitTens != 1)
+            System.out.print(kroner + " датская крона");
+        else
+            if ((digitOnes == 2 || digitOnes == 3 || digitOnes == 4) && digitTens != 1)
+                System.out.print(kroner + " датские кроны");
+            else
+                System.out.print(kroner + " датских крон");
+
     }
 }
