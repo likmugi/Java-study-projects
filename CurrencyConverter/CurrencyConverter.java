0ff862804cc3e9a1eb79a51d7a7df89f229c2514
Lika <shelley.bass.li@gmail.com>
2022-06-30 10:57:09 +0300

    Update CurrencyConverter
    Добавление в программу цикла для возможности многократной конвертации денежных сумм.

--- CurrencyConverter/CurrencyConverter.java
+++ CurrencyConverter/CurrencyConverter.java
@@ -2,35 +2,51 @@
     Проект. Часть 1.
     Напишите программу, которая конвертирует сумму денег из датских крон
     в российсие рубли по курсу покупки 13.82.
+
+    Часть 2.
+    Добавление в проект структуры выбора для принятия решени
+    об окончаниях входной валюты в зависимости от её значений.
+
+    Часть 3.
+    Добавление в программу цикл проверки правильности ввода количества конвертаций
+    и счетный цикл для многократных преобразований сумм.
  */
 
 import java.util.Scanner;
 
 public class CurrencyConverter {
+
     public static void main(String[] args) {
         final double ROUBLE_PER_DKRONER = 13.82; // курс покупки датской кроны
         int dkk; // сумма денег в датских кронах
         double rub; // сумма денег в российских рублях
+        int numberOfConversions; // количество планируемых конвертаций
 
         Scanner into = new Scanner(System.in);
 
-        // Получим сумму в кронах от пользователя
-        System.out.println("Введите сумму денег в кронах: ");
-        dkk = into.nextInt();
+        // Запрашиваем количество планируемых конвертаций
+        do {
+            System.out.println("Введите корректное количество конвертаций: ");
+            numberOfConversions = into.nextInt();
+        } while (numberOfConversions <= 0);
 
-        // Вычисляем сумму в рублях
-        rub = ROUBLE_PER_DKRONER * dkk;
+        while (numberOfConversions != 0) {
+            // Получим сумму в кронах от пользователя
+            System.out.println("\n" + "Введите сумму денег в кронах: ");
+            dkk = into.nextInt();
 
-        // Проверяем склонение и отображаем результат
-        checkDeclination(dkk);
-        System.out.println(" при переводе на рубли будет равна " + rub + ".");
+            // Вычисляем сумму в рублях
+            rub = ROUBLE_PER_DKRONER * dkk;
+
+            // Проверяем склонение и отображаем результат
+            checkDeclination(dkk);
+            System.out.println(" при переводе на рубли будет равна " + rub + ".");
+
+            numberOfConversions--; // Уменьшаем количество оставшихся конвертаций
+        }
+
     }
 
-    /*
-        Проект. Часть 2.
-        Добавление в проект структуры выбора для принятия решений
-        об окончаниях входной валюты в зависимости от её значений.
-     */
     public static void checkDeclination(int kroner) {
         int digitOnes = kroner % 10; // цифра разряда единиц
         int digitTens = kroner % 100 / 10; // цифра разряда десятков
@@ -38,10 +54,11 @@ public class CurrencyConverter {
         if (digitOnes == 1 && digitTens != 1)
             System.out.print(kroner + " датская крона");
         else
-            if ((digitOnes == 2 || digitOnes == 3 || digitOnes == 4) && digitTens != 1)
-                System.out.print(kroner + " датские кроны");
-            else
-                System.out.print(kroner + " датских крон");
+        if ((digitOnes == 2 || digitOnes == 3 || digitOnes == 4) && digitTens != 1)
+            System.out.print(kroner + " датские кроны");
+        else
+            System.out.print(kroner + " датских крон");
 
     }
+
 }
